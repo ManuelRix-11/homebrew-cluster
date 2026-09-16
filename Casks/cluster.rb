f55fb9a10a2dbe611888852c44f3fbc1282be3da
ManuelRix <62151163+ManuelRix-11@users.noreply.github.com>
2026-09-16 23:16:12 +0200

    Update cluster.rb

--- Casks/cluster.rb
+++ Casks/cluster.rb
@@ -1,6 +1,6 @@
 cask "cluster" do
   version "1.5.122"
-  sha256 "d3723648c5a3bc1453f85a276fed056b7aacff4c20dcad1989c7f15d6afba5a5"
+  sha256 "8b3d16ab6ac7fa6c9d12af89e1f3b939ce5c45af752445b8dcb4f7db693fd5f6"
 
   url "https://github.com/ManuelRix-11/Cluster-Releases/releases/download/v#{version}/Cluster-#{version}-arm64.dmg"
   name "Cluster"
